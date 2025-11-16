339be3f15ac6c97bbfe63a9ba94c195b51977618
Patricio <patriciojimenez838@gmail.com>
2025-11-15 22:22:52 -0300

    Primera estructura de carpetas

---
+++ src/main/java/com/duocdevs/asistrack/AsistrackApplication.java
@@ -0,0 +1,13 @@
+package com.duocdevs.asistrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duocdevs/asistrack/AsistrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duocdevs.asistrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
